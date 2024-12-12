92e0bc969577934fb24c96f689191dffe3726dd2
Kaur Kuut <strom@nevermore.ee>
2024-12-12 16:12:30 +0200

    Add test job.

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -2,3 +2,11 @@
 
 /// Super cool function.
 pub fn cool() {}
+
+#[cfg(test)]
+mod tests {
+    // CI will fail unless cargo nextest can execute at least one test per workspace.
+    // Delete this dummy test once we have an actual real test.
+    #[test]
+    fn dummy_test_until_we_have_a_real_test() {}
+}
