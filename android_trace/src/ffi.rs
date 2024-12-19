018b36733d6d8b8088224ebabcea3bed26c67daf
Kaur Kuut <strom@nevermore.ee>
2024-12-19 15:10:03 +0200

    Add android_trace.

---
+++ android_trace/src/ffi.rs
@@ -0,0 +1,155 @@
+// Copyright 2024 the Android Trace Authors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#[cfg(not(all(feature = "api_level_23", feature = "api_level_29")))]
+use core::{ffi::CStr, mem};
+
+use libc::c_char;
+
+/// # Safety
+///
+/// `func` must have been produced from a call to `dlsym` which is
+/// reasonably expected to have the right type.
+///
+/// All the preconditions from [`transmute_copy`](core::mem::transmute_copy) apply.
+#[cfg(not(all(feature = "api_level_23", feature = "api_level_29")))]
+#[allow(
+    unused_qualifications,
+    // reason = "These qualifications are used, because our MSRV is lower than 1.81"
+)]
+unsafe fn transmute_if_not_null<F>(func: *mut libc::c_void) -> Option<F> {
+    assert_eq!(
+        mem::size_of::<F>(),
+        mem::size_of::<*mut libc::c_void>(),
+        "transmute_copy is used because this function is generic"
+    );
+    if func.is_null() {
+        return None;
+    }
+    // Safety: The preconditions are guaranteed by the caller.
+    Some(unsafe { mem::transmute_copy::<*mut libc::c_void, F>(&func) })
+}
+
+#[link(name = "android", kind = "dylib")]
+#[cfg(target_os = "android")]
+#[cfg(feature = "api_level_23")]
+extern "C" {
+    #[link_name = "ATrace_beginSection"]
+    /// <https://developer.android.com/ndk/reference/group/tracing#atrace_beginsection>
+    pub(crate) fn atrace_begin_section_raw(section_name: *const c_char);
+
+    #[link_name = "ATrace_endSection"]
+    /// <https://developer.android.com/ndk/reference/group/tracing#atrace_endsection>
+    pub(crate) fn atrace_end_section_raw();
+
+    #[link_name = "ATrace_isEnabled"]
+    /// <https://developer.android.com/ndk/reference/group/tracing#atrace_isenabled>
+    pub(crate) fn atrace_is_enabled_raw() -> bool;
+}
+
+#[cfg(not(feature = "api_level_23"))]
+pub(crate) struct ATraceAPILevel23Methods {
+    pub(crate) begin_section: unsafe extern "C" fn(*const c_char),
+    pub(crate) end_section: unsafe extern "C" fn(),
+    pub(crate) is_enabled: unsafe extern "C" fn() -> bool,
+}
+
+// Link to Android in case the api_level_23 is disabled (i.e. we don't have the extern block above)
+// SAFETY: This is required for the calls to dlsym to be safe, ensuring that the accessed methods
+// don't get unlinked
+#[link(name = "android", kind = "dylib")]
+extern "C" {}
+
+#[cfg(not(feature = "api_level_23"))]
+impl ATraceAPILevel23Methods {
+    pub(crate) fn get() -> Option<&'static Self> {
+        use libc::RTLD_DEFAULT;
+        use std::sync::OnceLock;
+
+        static API_LEVEL_23_METHODS: OnceLock<Option<ATraceAPILevel23Methods>> = OnceLock::new();
+        API_LEVEL_23_METHODS
+            .get_or_init(|| {
+                let is_enabled = unsafe {
+                    const IS_ENABLED_NAME: &CStr = c"ATrace_isEnabled";
+                    // Safety: We're on Android, and have definitely linked to libandroid, so this function
+                    // should have the expected signature if present
+                    transmute_if_not_null(libc::dlsym(RTLD_DEFAULT, IS_ENABLED_NAME.as_ptr()))?
+                };
+                let begin_section = unsafe {
+                    const BEGIN_SECTION_NAME: &CStr = c"ATrace_beginSection";
+                    // Safety: As above
+                    transmute_if_not_null(libc::dlsym(RTLD_DEFAULT, BEGIN_SECTION_NAME.as_ptr()))?
+                };
+                let end_section = unsafe {
+                    const END_SECTION_NAME: &CStr = c"ATrace_endSection";
+                    // Safety: As above
+                    transmute_if_not_null(libc::dlsym(RTLD_DEFAULT, END_SECTION_NAME.as_ptr()))?
+                };
+                Some(Self {
+                    is_enabled,
+                    begin_section,
+                    end_section,
+                })
+            })
+            .as_ref()
+    }
+}
+
+#[link(name = "android", kind = "dylib")]
+#[cfg(target_os = "android")]
+#[cfg(feature = "api_level_29")]
+extern "C" {
+    #[link_name = "ATrace_beginAsyncSection"]
+    /// <https://developer.android.com/ndk/reference/group/tracing#atrace_beginasyncsection>
+    pub(crate) fn atrace_begin_async_section_raw(section_name: *const c_char, cookie: i32);
+
+    #[link_name = "ATrace_endAsyncSection"]
+    /// <https://developer.android.com/ndk/reference/group/tracing#atrace_endasyncsection>
+    pub(crate) fn atrace_end_async_section_raw(section_name: *const c_char, cookie: i32);
+
+    #[link_name = "ATrace_setCounter"]
+    //<https://developer.android.com/ndk/reference/group/tracing#atrace_setcounter>
+    pub(crate) fn atrace_set_counter_raw(counter_name: *const c_char, counter_value: i64);
+}
+
+#[cfg(not(feature = "api_level_29"))]
+pub(crate) struct ATraceAPILevel29Methods {
+    pub(crate) begin_async_section: unsafe extern "C" fn(*const c_char, i32),
+    pub(crate) end_async_section: unsafe extern "C" fn(*const c_char, i32),
+    pub(crate) set_counter: unsafe extern "C" fn(*const c_char, i64),
+}
+
+#[cfg(not(feature = "api_level_29"))]
+impl ATraceAPILevel29Methods {
+    pub(crate) fn get() -> Option<&'static Self> {
+        use libc::RTLD_DEFAULT;
+        use std::sync::OnceLock;
+
+        static API_LEVEL_29_METHODS: OnceLock<Option<ATraceAPILevel29Methods>> = OnceLock::new();
+        API_LEVEL_29_METHODS
+            .get_or_init(|| {
+                let set_counter = unsafe {
+                    const SET_COUNTER_NAME: &CStr = c"ATrace_setCounter";
+                    // Safety: We're on Android, and have definitely linked to libandroid, so this function
+                    // should have the expected signature if present
+                    transmute_if_not_null(libc::dlsym(RTLD_DEFAULT, SET_COUNTER_NAME.as_ptr()))?
+                };
+                let begin_async_section = unsafe {
+                    const BEGIN_SECTION_NAME: &CStr = c"ATrace_beginAsyncSection";
+                    // Safety: As above
+                    transmute_if_not_null(libc::dlsym(RTLD_DEFAULT, BEGIN_SECTION_NAME.as_ptr()))?
+                };
+                let end_async_section = unsafe {
+                    const END_SECTION_NAME: &CStr = c"ATrace_endAsyncSection";
+                    // Safety: As above
+                    transmute_if_not_null(libc::dlsym(RTLD_DEFAULT, END_SECTION_NAME.as_ptr()))?
+                };
+                Some(Self {
+                    set_counter,
+                    begin_async_section,
+                    end_async_section,
+                })
+            })
+            .as_ref()
+    }
+}
---
+++ android_trace/src/lib.rs
@@ -0,0 +1,290 @@
+// Copyright 2024 the Android Trace Authors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! Android trace
+//! ```
+//! use android_trace::AndroidTrace;
+//! let trace = AndroidTrace::new();
+//!
+//! // If the `is_enabled` method isn't available, we also shouldn't trace
+//! let should_trace = trace.is_enabled().unwrap_or(false);
+//! if should_trace {
+//!   trace.begin_section(c"My expensive calculation");
+//! }
+//!
+//! // ...performing an expensive calculation
+//!
+//! if should_trace {
+//!   trace.end_section();
+//! }
+//! ```
+
+#[cfg(not(feature = "api_level_23"))]
+use ffi::ATraceAPILevel23Methods;
+#[cfg(not(feature = "api_level_29"))]
+use ffi::ATraceAPILevel29Methods;
+
+use core::ffi::CStr;
+use std::fmt::Debug;
+
+#[cfg(not(target_os = "android"))]
+compile_error!(
+    r#"android_trace only supports Android. If you are depending on it, ensure that it is within
+    [target.'cfg(target_os = "android")'.dependencies]
+    in your Cargo.toml"#
+);
+
+mod ffi;
+
+/// A handle to the available NDK tracing functions
+///
+/// All access is thread safe
+#[derive(Clone)]
+pub struct AndroidTrace {
+    #[cfg(not(feature = "api_level_23"))]
+    api_level_23: Option<&'static ATraceAPILevel23Methods>,
+    #[cfg(not(feature = "api_level_29"))]
+    api_level_29: Option<&'static ATraceAPILevel29Methods>,
+}
+
+impl AndroidTrace {
+    /// Get a handle to all of the NDK tracing functions available on this device.
+    ///
+    /// This should be expected to have a low runtime cost.
+    ///
+    /// Can subsequently be used across multiple threads
+    pub fn new() -> Self {
+        Self {
+            #[cfg(not(feature = "api_level_23"))]
+            api_level_23: ATraceAPILevel23Methods::get(),
+            #[cfg(not(feature = "api_level_29"))]
+            api_level_29: ATraceAPILevel29Methods::get(),
+        }
+    }
+
+    /// Get a handle to the first level of the NDK tracing functions, where available
+    /// on this device.
+    ///
+    /// This should be expected to have a low runtime cost.
+    pub fn new_downlevel() -> Self {
+        Self {
+            #[cfg(not(feature = "api_level_23"))]
+            api_level_23: ATraceAPILevel23Methods::get(),
+            #[cfg(not(feature = "api_level_29"))]
+            api_level_29: None,
+        }
+    }
+
+    /// Returns Some(true) if tracing through Android Trace is enabled (and Some(false) if it is disabled).
+    /// This value is *not* guaranteed to have the same value over time.
+    /// Tracing may begin and end during execution.
+    /// Note that the Android platform does not provide any non-polling method for determining whether
+    /// this tracing is enabled.
+    ///
+    /// If `ATrace_isEnabled` is not available, returns None.
+    /// This means that none of the tracing methods will have any effect during this program execution,
+    /// and so can be skipped.
+    ///
+    /// Calls [`ATrace_isEnabled`](https://developer.android.com/ndk/reference/group/tracing#atrace_isenabled)
+    /// if available. This is only available since Android API level 23. If the `api_level_23` feature is not
+    /// enabled, this will attempt to access a dynamically linked version of the underlying function.
+    /// Please note that `api_level_23` is a default feature.
+    #[doc(alias = "ATrace_isEnabled")]
+    #[must_use = "Detecting if tracing is enabled has no side effects"]
+    pub fn is_enabled(&self) -> Option<bool> {
+        // SAFETY: No preconditions
+        #[cfg(feature = "api_level_23")]
+        unsafe {
+            Some(ffi::atrace_is_enabled_raw())
+        }
+        #[cfg(not(feature = "api_level_23"))]
+        if let Some(methods) = self.api_level_23 {
+            // Safety: No preconditions
+            let result = unsafe { (methods.is_enabled)() };
+            Some(result)
+        } else {
+            None
+        }
+    }
+
+    /// Writes a tracing message to indicate that the given section of code has begun.
+    ///
+    /// This should be followed by a call to [`Self::end_section`] on the same thread, to close the
+    /// opened section.
+    ///
+    /// Calls [`ATrace_beginSection`](https://developer.android.com/ndk/reference/group/tracing#atrace_beginsection)
+    /// if available. This is only available since Android API level 23. If the `api_level_23` feature is not
+    /// enabled, this will attempt to access a dynamically linked version of the underlying function.
+    /// Please note that `api_level_23` is a default feature.
+    ///
+    /// If `ATrace_beginSection` is not available, this has no effect.
+    #[doc(alias = "ATrace_beginSection")]
+    pub fn begin_section(&self, section_name: &CStr) {
+        #[cfg(feature = "api_level_23")]
+        unsafe {
+            // SAFETY: section_name is a valid C string
+            ffi::atrace_begin_section_raw(section_name.as_ptr());
+        }
+        #[cfg(not(feature = "api_level_23"))]
+        if let Some(methods) = self.api_level_23 {
+            // SAFETY: section_name is a valid C string
+            unsafe { (methods.begin_section)(section_name.as_ptr()) }
+        }
+    }
+
+    /// Writes a tracing message to indicate that a given section of code has ended.
+    ///
+    /// This should follow a call to [`Self::begin_section`] on the same thread, as
+    /// this will be closing an opened section.
+    ///
+    /// Calls [`ATrace_endSection`](https://developer.android.com/ndk/reference/group/tracing#atrace_endsection)
+    /// if available. This is only available since Android API level 23. If the `api_level_23` feature is not
+    /// enabled, this will attempt to access a dynamically linked version of the underlying function.
+    /// Please note that `api_level_23` is a default feature
+    ///
+    /// If `ATrace_endSection` is not available, this has no effect.
+    #[doc(alias = "ATrace_endSection")]
+    pub fn end_section(&self) {
+        // SAFETY: No preconditions.
+        #[cfg(feature = "api_level_23")]
+        unsafe {
+            ffi::atrace_end_section_raw();
+        }
+        #[cfg(not(feature = "api_level_23"))]
+        if let Some(methods) = self.api_level_23 {
+            // Safety: No preconditions
+            unsafe { (methods.end_section)() }
+        }
+    }
+
+    /// Writes a tracing message to indicate that a given section of code has begun.
+    ///
+    /// This should be followed by a call to [`Self::end_async_section`] with the same `section_name` and `cookie`,
+    /// although this subsequent call can occur on any thread.
+    ///
+    /// Calls [`ATrace_beginAsyncSection`](https://developer.android.com/ndk/reference/group/tracing#atrace_beginasyncsection)
+    /// if available. This is only available since Android API level 29. If the `api_level_29` feature is not
+    /// enabled, this will attempt to access a dynamically linked version of the underlying function.
+    ///
+    /// If `ATrace_beginAsyncSection` is not available, this has no effect.
+    #[doc(alias = "ATrace_beginAsyncSection")]
+    pub fn begin_async_section(&self, section_name: &CStr, cookie: i32) -> Option<()> {
+        // SAFETY: No preconditions.
+        #[cfg(feature = "api_level_29")]
+        unsafe {
+            ffi::atrace_begin_async_section_raw(section_name.as_ptr(), cookie);
+            Some(())
+        }
+        #[cfg(not(feature = "api_level_29"))]
+        if let Some(methods) = self.api_level_29 {
+            // Safety: No preconditions
+            unsafe { (methods.begin_async_section)(section_name.as_ptr(), cookie) }
+            Some(())
+        } else {
+            None
+        }
+    }
+
+    /// Writes a tracing message to indicate that a given section of code has ended
+    ///
+    /// This should follow a call to [`Self::begin_async_section`] with the same `section_name` and `cookie`,
+    /// although this call can occur on any thread
+    ///
+    /// Calls [`ATrace_endAsyncSection`](https://developer.android.com/ndk/reference/group/tracing#atrace_endasyncsection)
+    /// if available. This is only available since Android API level 29. If the `api_level_29` feature is not
+    /// enabled, this will attempt to access a dynamically linked version of the underlying function.
+    ///
+    /// If `ATrace_endAsyncSection` is not available, this has no effect
+    #[doc(alias = "ATrace_endAsyncSection")]
+    pub fn end_async_section(&self, section_name: &CStr, cookie: i32) -> Option<()> {
+        // SAFETY: No preconditions.
+        #[cfg(feature = "api_level_29")]
+        unsafe {
+            ffi::atrace_end_async_section_raw(section_name.as_ptr(), cookie);
+            Some(())
+        }
+        #[cfg(not(feature = "api_level_29"))]
+        if let Some(methods) = self.api_level_29 {
+            // Safety: No preconditions
+            unsafe { (methods.end_async_section)(section_name.as_ptr(), cookie) }
+            Some(())
+        } else {
+            None
+        }
+    }
+
+    /// Whether the [`Self::set_counter`], [`Self::begin_async_section`] and [`Self::end_async_section`]
+    /// might do anything.
+    ///
+    /// This value *will* be the same across multiple calls (on this [`AndroidTrace`] instance -
+    /// see [`Self::new_downlevel`]), and so can be used as an early-fastpath feature.
+    ///
+    /// Note that you should also call [`Self::is_enabled`] if calculating
+    /// an individual value to pass to the corresponding functions will be expensive
+    pub fn could_use_api_level_29(&self) -> bool {
+        #[cfg(not(feature = "api_level_29"))]
+        return self.api_level_29.is_some();
+        #[cfg(feature = "api_level_29")]
+        true
+    }
+
+    /// Writes a trace message to indicate that the counter with the given name has the given value.
+    ///
+    /// Calls [`ATrace_setCounter`](https://developer.android.com/ndk/reference/group/tracing#atrace_setcounter)
+    /// if available. This is only available since Android API level 29. If the `api_level_29` feature is not
+    /// enabled, this will attempt to access a dynamically linked version of the underlying function.
+    ///
+    /// If `ATrace_endAsyncSection` is not available, this has no effect
+    #[doc(alias = "ATrace_setCounter")]
+    pub fn set_counter(&self, counter_name: &CStr, value: i64) -> Option<()> {
+        // SAFETY: No preconditions.
+        #[cfg(feature = "api_level_29")]
+        unsafe {
+            ffi::atrace_set_counter_raw(counter_name.as_ptr(), value);
+            Some(())
+        }
+        #[cfg(not(feature = "api_level_29"))]
+        if let Some(methods) = self.api_level_29 {
+            // Safety: No preconditions
+            unsafe { (methods.set_counter)(counter_name.as_ptr(), value) }
+            Some(())
+        } else {
+            None
+        }
+    }
+}
+
+impl Default for AndroidTrace {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Debug for AndroidTrace {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        #[cfg(not(feature = "api_level_23"))]
+        let has_level_23 = self.api_level_23.is_some();
+        #[cfg(feature = "api_level_23")]
+        let has_level_23 = true;
+        #[cfg(not(feature = "api_level_29"))]
+        let has_level_29 = self.api_level_29.is_some();
+        #[cfg(feature = "api_level_29")]
+        let has_level_29 = true;
+        let api_level = match (has_level_29, has_level_23) {
+            (true, true) => "29",
+            (false, true) => "23",
+            (false, false) => "None",
+            (true, false) => "Unexpected: 29 but not 23",
+        };
+        f.debug_struct("AndroidTrace")
+            .field("api_level", &api_level)
+            .finish()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use static_assertions as sa;
+    sa::assert_impl_all!(AndroidTrace: Send, Sync);
+}
